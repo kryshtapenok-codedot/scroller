aa6b01c37809e0dc1118d80ecd7b2077734b3ed9
Flaming <kryshtapenok.codedot@gmail.com>
2021-01-25 14:13:54 +0300

    добавление возможности добавлять блоки после и до scroller

--- js.js
+++ js.js
@@ -1,5 +1,56 @@
-const test = (id)=>{
-    const elem = document.getElementById(id);
-    elem.style.setProperty("--newtransition","-"+(pageYOffset*1.6)/2000*100+"%")
+    const test = document.getElementById('1');
+    const topcoord = test.getBoundingClientRect().top;
+    const bottomcoord  = test.getBoundingClientRect().bottom;
+    let koord = 0;
+
+ 
+   
+    
+
+const scroller = (id)=>{ 
+/*     const scrollablecontent = document.getElementById(id);
+ 
+    if(test.getBoundingClientRect().top <600 & test.getBoundingClientRect().bottom>0){
+    scrollablecontent.style.setProperty("--newtransition","-"+(Math.abs(pageYOffset-topcoord)*1.6)/2000*100+"%")
+    console.log(test.getBoundingClientRect().top)
+    console.log(pageYOffset)}
+    else{
+        scrollablecontent.style.setProperty("--nawtransition","0%")
+    } */
+
+
+    const scrollablecontent1 = document.getElementById(id);
+    if (topcoord-pageYOffset <= 400){
+        console.log(test.getBoundingClientRect().top) 
+        scrollablecontent1.style.position = "fixed"
+        scrollablecontent1.style.setProperty("--newtransitionX","-110%")
+        scrollablecontent1.style.top = "72%"
+    if (topcoord- pageYOffset>0){
+    koord = Math.abs(topcoord - pageYOffset-400);
+    scrollablecontent1.style.setProperty("--newtransitionY","-"+(koord*1.6)/2000*100+"%")
+    console.log((koord*1.6)/2000*100);
+    }
+    else{
+        if (test.getBoundingClientRect().bottom>980){
+        scrollablecontent1.style.setProperty("--newtransitionY","-"+((pageYOffset-topcoord+koord)*1.6)/2000*100+"%")
+    }
+    else{
+        scrollablecontent1.style.position = "absolute";
+        scrollablecontent1.style.setProperty("--newtransitionX","-110%") 
+        scrollablecontent1.style.top = "86%"
+    }
 }
-window.addEventListener("scroll",() => test('2'))
+    
+    
+}
+else{
+    
+    scrollablecontent1.style.position = "absolute"
+    scrollablecontent1.style.setProperty("--newtransitionX","-110%")
+    scrollablecontent1.style.top = "300px"  
+
+    
+}
+}
+window.addEventListener("scroll",() => scroller('2'))
+
